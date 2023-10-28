16bc4c6def3e2dd055978a10316cae61932e42bf
CheeseMans <136197809+JSDexDB@users.noreply.github.com>
2023-10-28 02:37:52 -0400

    cors anywhere

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var dex = require('@pkmn/dex');
 var generations = require('@pkmn/data');
 var smogon = require('@pkmn/smogon');
 var LL_smogon = require('smogon')
+var cors_anywhere = require('cors-anywhere')
 
 global.window.dex = dex
 global.window.generations = generations
 global.window.smogon = smogon
 global.window.LL_smogon = LL_smogon
+global.window.cors_anywhere = cors_anywhere
