64227effe5151f0dab45e9320230f2f0515445aa
CheeseMans <136197809+JSDexDB@users.noreply.github.com>
2024-04-01 01:53:47 -0400

    Added website

--- src/App.js
+++ src/App.js
@@ -6,12 +6,76 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { BarChart } from '@mui/x-charts/BarChart';
 import {useSpring, animated} from 'react-spring';
+import { alignProperty } from '@mui/material/styles/cssUtils';
+import {Text} from 'react-native';
+import { evaluateArgument } from './data_analysis';
 
 const MAX_PKMN = 1025;
 var loaded_pkmn = 0;
 var inline_loaded = 0;
 var startedLoading = false;
+var activeID = 1;
+var activeMon = [];
+var monForms = {};
+var inSearch = false;
+var activeData = [];
+var pokeStats= [];
+var statNames = {
+  'hp':'HP',
+  'attack':'Atk',
+  'defense':'Def',
+  'special-attack':'SpA',
+  'special-defense':'SpD',
+  'speed':'Spe'
+}
+var typeColours = {
+  'normal': '#ABAB9B',
+  'fire': '#FF4422',
+  'water': '#3399FF',
+  'electric': '#FFCC33',
+  'grass': '#77CC55',
+  'ice': '#66CCFF',
+  'fighting': '#BB5544',
+  'poison': '#AA5599',
+  'ground': '#DDBB55',
+  'flying': '#8899FF',
+  'psychic': '#FF5599',
+  'bug': '#AABB22',
+  'rock': '#BBAA66',
+  'ghost': '#6666BB',
+  'dragon': '#7766EE',
+  'dark': '#775544',
+  'steel': '#AAAABB',
+  'fairy': '#EE99EE'
+}
+var statNameVals = [
+  'HP',
+  'Atk',
+  'Def',
+  'SpA',
+  'SpD',
+  'Spe'
+]
+var form_whitelist = 
+    {
+        "25": [1, 2, 4, 5, 14, 16],
+        "658": [2],
+        "718": [1,2,3],
+        "735": [],
+        "738": [],
+        "743": [],
+        "752": [],
+        "754": [],
+        "758": [],
+        "774": [7,8,9,10,11,12],
+        "777": [],
+        "778": [],
+        "784": [],
+        "1007": [],
+        "1008": []
+    }
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -50,12 +114,107 @@ function titleCase(/**@type String */str)
     return base.charAt(0).toUpperCase() + base.slice(1);
 }
 
+function splitTitleCase(/**@type String */str)
+{
+    var parts = str.split("-");
+    var final = ""
+    for(var i = 0; i < parts.length; i++)
+    {
+        final += titleCase(parts[i]);
+        if(i < parts.length - 1)
+        {
+            final += " ";
+        }
+    }
+    return final;
+}
+
+function analysis(searchQuery)
+{
+    var chosenData = []
+    if(searchQuery == "")
+    {
+        chosenData = data;
+    }
+    else
+    {
+        var tempList = []
+        var args = searchQuery.trimEnd().trimStart().split(" ");
+        for(var i = 0; i < args.length; i++)
+        {
+            tempList = tempList.concat(evaluateArgument(args[i], data, monForms));
+        }
+        var endList = []
+        if(tempList.length == 0)
+        {
+            endList = data;
+        }
+        else
+        {
+            var sortMode = "AND"
+            switch(sortMode)
+            {
+                case "OR":
+                    endList = [...new Set(tempList)];
+                    break;
+                case "AND":
+                    var counts = {};
+                    var names = {};
+
+                    for (var i = 0; i < tempList.length; i++) 
+                    {
+                        var ic = tempList[i];
+                        console.log("ic: ",ic);
+                        counts[ic[0].name] = counts[ic[0].name] ? counts[ic[0].name] + 1 : 1;
+                        names[ic[0].name] = ic;
+                        console.log(counts[ic[0].name]);
+                    }
+                    console.log(counts);
+                    var keys = Object.keys(counts);
+                    for(var c = 0; c < keys.length; c++)
+                    {
+                        var k = keys[c];
+                        console.log(k + ": " + counts[k])
+                        if(counts[k] == args.length){endList.push(names[k]);}
+                    }
+                    break;
+                  default:
+                    break;
+            }
+            
+        }
+        chosenData = endList;
+    }
+    return chosenData;
+}
+
+function pokeSprite(ID)
+{
+  return "data/sprites/"+ID+"/front_default.png";
+}
+
+function search(query, dataProccer)
+{
+  console.log("query:",query);
+  activeData = analysis(query);
+  if(activeData.length == data.length)
+  {
+    inSearch = false;
+  }
+  else
+  {
+    inSearch = true;
+  }
+  dataProccer(activeData);
+}
+
+
+
 
 
 function pokeIcon(ID)
 {
   var pokedata = data[ID]
-  console.log(pokedata)
   return (
   <Grid item xs={2} sm={4} md={4} key={ID} id={ID}>
     <Item><img src={process.env.PUBLIC_URL + pokedata[2]} style={{alignContent: "center"}} alt="pokemon data" key={ID}></img>{titleCase(pokedata[0].name)}</Item>
@@ -91,6 +250,26 @@ render() {
 }
 }
 
+function activateOverlay(ID, updateMethod)
+{
+  if(activeID == ID)
+  {
+    activeMon = [];
+    activeID = 0;
+    updateMethod(activeMon);
+    return;
+  }
+  if(activeMon.length == 0)
+  {
+    var p = document.querySelector('.pokePanel');
+    p.style.animationName = 'none';
+    requestAnimationFrame(() => {p.style.animationName = ''});
+  }
+  activeID = ID;
+  activeMon = [data[activeID-1]];
+  updateMethod(activeMon);
+}
+
 var data = []
 async function load_data(updateLoader, lD)
 {
@@ -106,11 +285,72 @@ async function load_data(updateLoader, lD)
       loaded_pkmn++;
       var mon_data = await getData(i);
       data.push(mon_data);
+      monForms[toString(mon_data[3])] = [];
+      var v = Object.keys(mon_data[1].varieties)
+      for(var i = 1; i < v.length; i++)
+        {
+            if(Object.keys(form_whitelist).includes(toString(mon_data[3])) && !form_whitelist[mon_data[3]].includes(i)){continue;}
+            /**@type String */
+            var fullName = mon_data[1].varieties[v[i]]["pokemon"]["name"]
+            var formName = fullName.replace(mon_data[0]["species"]["name"] + "-","")
+            var path = "data/sprites/"+mon_data[3]+"/"+formName+"/front_default.png"
+            var form_data_response = await fetch("data/api/"+mon_data[3]+"/"+formName+"/api.json").then((response) => response.json())
+            var formObj = {
+                name: titleCase(formName),
+                img: path,
+                data: form_data_response
+            }
+            monForms[toString(data[3])].push(formObj);
+        }
+      
+      if(inSearch == false)
+      {
+        activeData = data;
+      }
       updateLoader(data);
-      await delay(2);
+      if(activeMon.length > 0)
+      {
+        await delay(200);
+      }
+      else
+      {
+        await delay(2);
+      }
+      
     }
   }
 }
+
+class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.reset();
+  }
+
+  reset() {
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  render() {
+    return (
+      // ...
+      <input style={{borderRadius: "20px", zIndex:12, position: 'fixed', justifySelf: "center", borderWidth:"1px", width:"75%", margin:"auto", display:"block", transform: "translate(0px, 10px)", paddingLeft: "20px"}}
+      type="search" onChange={event => search.bind(this, event.target.value)}/>
+      // ...
+    );
+  }
+
+  updateInputValue(evt) {
+    const val = evt.target.value;
+    // ...       
+    this.setState({
+      inputValue: val
+    });
+  }
+}
+
 
 function App() {
 /*   console.log(data);
@@ -120,35 +360,95 @@ function App() {
     grid_rows.push(pokeIcon(i));
   } */
   const [loadedData, setLoad] = useState([]);
+  const [acMon, setMon] = useState([]);
+  const [queryText, setQuery] = useState('');
   
   load_data(setLoad, loadedData);
-  console.log(inline_loaded);
   if(inline_loaded != data.length)
   {
     setLoad(data);
     inline_loaded = data.length; 
-    console.log("inline:",inline_loaded, loadedData);
   }
-  React.useEffect(() => {
-    console.log("loadedData: ", loadedData);
-  })
-  const props = useSpring({
-    to: {opacity: 1},
-    from: {opacity: 0},
-    reset: true
-  })
+  var loadingText = <h3></h3>;
+  if(loaded_pkmn < MAX_PKMN)
+  {
+    loadingText = <div><h3 style={{textAlign: "center", alignContent:"center",justifyContent:"center",position: "fixed", top:"50%", left:"50%",transform:"translate(-50%, -50%)", fontSize:"64px", margin: "auto",zIndex: 11}}>Loading: {Math.round(100*loaded_pkmn/MAX_PKMN)}%</h3>
+    <h6 style={{textAlign: "center", alignContent:"center",justifyContent:"center",position: "fixed", top:"57%", left:"50%",transform:"translate(-50%, -50%)", fontSize:"16px", margin: "auto",zIndex: 11}}>(If loading is stuck, reload the page)</h6></div>;
+  }
+
+  var toRender = []
+  if(loaded_pkmn >= MAX_PKMN)
+  {
+    toRender = activeData;
+  }
+  var pokeiconPanel = <p></p>;
+  if(loaded_pkmn >= activeID && activeMon.length > 0)
+  {
+    var pokestats = []
+    var curmon = activeMon[0];
+    for(var i = 0; i < 6; i++)
+    {
+      pokestats.push(curmon[0].stats[i].base_stat);
+    }
+    /** @type {Array} */
+    var poketypes = curmon[0].types;
+    var pokeabilities = curmon[0].abilities;
+    pokeiconPanel = activeMon.map(mon => 
+      <Item style={{display:"grid"}}>
+        <Item style={{width:"5%", justifySelf: "right"}} className='button-style' fontFamily="bwFont" onClick={activateOverlay.bind(this, activeID, setMon)}>Close</Item>
+        
+        <Grid container style={{justifyContent: "center", width:"100%", display:"grid", gridAutoFlow: "column", gridAutoRows: "max-content", gridAutoColumns: "33%"}} columns={3} row={1}>
+        <Grid item style={{width:"100%", alignSelf:"center", height: "200px"}}> 
+            <BarChart
+            yAxis={[{ scaleType: 'band', data: statNameVals, max:225}]}
+            xAxis={[{max:225}]}
+            series={[{data: pokestats, color: "purple"}]}
+            layout="horizontal"
+            />
+          </Grid>
+          
+          <Grid item>
+            <img src={process.env.PUBLIC_URL + mon[2]} style={{justifySelf: "center", imageRendering: "pixelated", width:"40%"}} alt="pokemon data"></img>
+            
+          </Grid>
+          <Grid item display={"flex-grid"} justifyContent={"center"} alignContent={"center"}>
+              <h1 fontFamily={"bwFont"}><Text style={{ fontSize: "32px"}}><b>{splitTitleCase(mon[0].name)}</b></Text></h1>
+              <Text style={{fontSize: "12px", color:("black")}}>Type:</Text>
+              <br style={{display:"block"}}></br>
+              <div style={{margin:"10px", justifyContent: "center", display: "grid", gridAutoFlow: "column", columnWidth: "100%", gridGap: "10px", tableLayout: "fixed"}}>
+              {poketypes.map(ptype => 
+                <Item style={{width: "100px", alignSelf:"center", backgroundColor:typeColours[ptype.type.name]}}><Text style={{fontSize: "20px", color:"white"}}>{titleCase(ptype.type.name) + "  "}</Text></Item>
+                )}
+              </div>
+              <br></br>
+              <br></br>
+              <Text style={{fontSize: "12px", color:("black")}}>Abilities:</Text>
+              <br></br>
+              {pokeabilities.map(pabl => 
+                <Text style={{fontSize: "18px", color:(pabl.is_hidden == false ? "black" : "#C8AE0F")}}>{splitTitleCase(pabl.ability.name) + "   "}</Text>
+                )}
+          </Grid>
+        </Grid>
+     </Item>)
+  }
+
   return (
-    <div className="App">
-      
-      <h1 style={{textAlign: "center"}}>JSDex</h1>
+    <div className="App" style={{display:"grid"}}>
+      <input style={{borderRadius: "20px", zIndex:12, position: 'fixed', justifySelf: "center", borderWidth:"1px", width:"75%", margin:"auto", display:"block", transform: "translate(0px, 10px)", paddingLeft: "20px"}}
+      type="search" onChange={event => search(event.target.value, setLoad)}/>
+      {loadingText}
+      <div className='pokePanel' fontFamily={"bwFont"} style={{zIndex: 10}}>
+        {pokeiconPanel}
+      </div>
+      <br></br>
+      <h2 style={{textAlign: "center", fontFamily:"bwFont"}}>PokéSearch</h2>
       <body>
       <Grid container spacing={1}>
-        {data.map(poke => 
+        {toRender.map(poke => 
         
-        <Grid item xs={1} sm={4} md={1.5} key={poke[3]}>
-          <Item style={{}} className='load-style'>
+        <Grid item xs={4} sm={2} md={1.5} lg={1} key={poke[3]} className='hover-style'>
+          <Item style={{}} className='load-style' onClick={activateOverlay.bind(this, poke[3], setMon)}>
           <img src={process.env.PUBLIC_URL + poke[2]} style={{alignContent: "center"}} alt="pokemon data"></img>
-          <h3 style={{textAlign: "center"}}>{titleCase(poke[0].name)}</h3>
           </Item>
         </Grid>
         )}
---
+++ src/data_analysis.js
@@ -0,0 +1,327 @@
+var globalSortMode = "AND";
+
+function count(/**@type String */str, /**@type String */seg)
+{
+    return (str.match(new RegExp(str)) || []).length;
+}
+
+function setMode(value)
+{
+    if(value == "OR"){globalSortMode = "OR";}
+    if(value == "AND"){globalSortMode = "AND";}
+}
+
+function getGen(ID)
+{
+    if(ID < 152)
+    {
+        return 1;
+    }
+    else if (ID < 252)
+    {
+        return 2;
+    }
+    else if(ID < 387)
+    {
+        return 3;
+    }
+    else if(ID < 494)
+    {
+        return 4;
+    }
+    else if(ID < 650)
+    {
+        return 5;
+    }
+    else if(ID < 722)
+    {
+        return 6;
+    }
+    else if(ID < 810)
+    {
+        return 7;
+    }
+    else if(ID < 906)
+    {
+        return 8;
+    }
+    else
+    {
+        return 9;
+    }
+}
+
+function checkWeight(icon, comp, value)
+{
+    switch(comp)
+    {
+        case "=":
+            return icon.data.weight == value;
+        case ">":
+            return icon.data.weight > value;
+        case "<":
+            return icon.data.weight < value;
+    }
+}
+
+function checkGeneration(icon, comp, str_value)
+{
+    var value = Number(str_value)
+    var gen = getGen(icon.ID);
+    switch(comp)
+    {
+        case "=":
+            return gen == value;
+        case ">":
+            return gen > value;
+        case "<":
+            return gen < value;
+    }
+}
+
+function checkID(icon, comp, str_value)
+{
+    var value = Number(str_value)
+    var icID = icon.ID;
+    switch(comp)
+    {
+        case "=":
+            return icID == value;
+        case ">":
+            return icID > value;
+        case "<":
+            return icID < value;
+    }
+}
+
+function checkForms(icon, comp, str_value)
+{
+    var value = Number(str_value) - 1
+    switch(comp)
+    {
+        case "=":
+            return icon.forms.length == value;
+        case ">":
+            return icon.forms.length > value;
+        case "<":
+            return icon.forms.length < value;
+    }
+}
+
+function checkLearnset(icon, comp, str_value)
+{
+    if(comp == "=")
+    {
+        console.log(icon.data.moves)
+        return icon.data.moves.some(x => (x.move.name == str_value.toLowerCase() || x.move.name.replace("-"," ") == str_value.toLowerCase()));
+    }
+    return false;
+}
+
+
+function checkType(icon, comp, value)
+{
+    if(comp == "=")
+    {
+        if(icon.data.types.length == 1)
+        {
+            return icon.data.types[0].type.name == value
+        }
+        else
+        {
+            return icon.data.types[0].type.name == value || icon.data.types[1].type.name == value;
+        }
+        
+    }
+    return false;
+}
+
+function checkName(icon, comp, value)
+{
+    if(comp == "=")
+    {
+        return icon.data.name == value;
+    }
+    else
+    {
+        return icon.data.name.includes(value);
+    }
+}
+
+function checkCol(icon, comp, value)
+{
+    if(comp == "=")
+    {
+        return icon.spec_data.color.name == value;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function getBST(stats)
+{
+    var total = 0;
+    for(var i = 0; i < 6; i++)
+    {
+        total += stats[i].base_stat
+    }
+    return total;
+}
+
+function checkStat(icon, comp, str_value, statID)
+{
+    var value = Number(str_value)
+    if(statID == 6) //BST
+    {
+        var bst = getBST(icon.data.stats);
+        switch(comp)
+        {
+            case "=":
+                return bst == value;
+            case ">":
+                return bst > value;
+            case "<":
+                return bst < value;
+        }
+        return false;
+    }
+    switch(comp)
+    {
+        case "=":
+            return icon.data.stats[statID].base_stat == value;
+        case ">":
+            return icon.data.stats[statID].base_stat > value;
+        case "<":
+            return icon.data.stats[statID].base_stat < value;
+    }
+    return false;
+}
+
+function checkHP(icon, comp, value){return checkStat(icon, comp, value, 0);}
+function checkAtk(icon, comp, value){return checkStat(icon, comp, value, 1);}
+function checkDef(icon, comp, value){return checkStat(icon, comp, value, 2);}
+function checkSpA(icon, comp, value){return checkStat(icon, comp, value, 3);}
+function checkSpD(icon, comp, value){return checkStat(icon, comp, value, 4);}
+function checkSpe(icon, comp, value){return checkStat(icon, comp, value, 5);}
+function checkBST(icon, comp, value){return checkStat(icon, comp, value, 6);}
+
+function checkCR(icon, comp, value)
+{
+    switch(comp)
+    {
+        case "=":
+            return icon.spec_data.capture_rate == value;
+        case ">":
+            return icon.spec_data.capture_rate > value;
+        case "<":
+            return icon.spec_data.capture_rate < value;
+    }
+}
+
+var operators = 
+{
+    "name":checkName,
+
+    "type":checkType,
+
+    "color":checkCol,
+    "colour":checkCol,
+
+    "weight":checkWeight,
+
+    "hp":checkHP,
+
+    "atk":checkAtk,
+
+    "def":checkDef,
+
+    "spa":checkSpA,
+
+    "spd":checkSpD,
+
+    "spe":checkSpe,
+    "speed":checkSpe,
+
+    "bst":checkBST,
+    "basestattotal":checkBST,
+    "base_stat_total":checkBST,
+
+    "capture":checkCR,
+    "capturerate":checkCR,
+    "capture_rate":checkCR,
+
+    "forms":checkForms,
+
+    "gen":checkGeneration,
+
+    "id":checkID,
+    "natdex":checkID,
+
+    "learns":checkLearnset
+}
+
+var desc =
+{
+    "checkName":"The name of the Pokemon's default form (i.e: Pikachu, Zoroark, Dunsparce)",
+    "checkType":"The elemental type(s) of the Pokemon (i.e: Fire, Water, Grass)",
+    "checkCol":"The internally specified colour of the Pokemon (i.e: Green, Purple, White)",
+    "checkWeight":"The internally specified weight of the Pokemon (i.e: 300, 5, 999)",
+    "checkHP":"The base stat for the Pokemon's HP stat",
+    "checkAtk":"The base stat for the Pokemon's ATTACK stat",
+    "checkDef":"The base stat for the Pokemon's DEFENSE stat",
+    "checkSpA":"The base stat for the Pokemon's SPECIAL ATTACK stat",
+    "checkSpD":"The base stat for the Pokemon's SPECIAL DEFENSE stat",
+    "checkSpe":"The base stat for the Pokemon's SPEED stat",
+    "checkBST":"The base stat total of a Pokemon (The sum of all of its base stats for HP, Atk, SpA, Def, SpD, Spe)",
+    "checkCR":"The internally specified capture rate of a Pokemon (i.e: 3, 255, 37)",
+    "checkForms":"The number of forms a Pokemon has (Including its default form)",
+    "checkGeneration":"The generation of mainline Pokemon games that the Pokemon was introduced in (i.e: 1, 5, 8)",
+    "checkID":"The national dex number of the Pokemon (i.e: 1, 76, 904)",
+    "checkLearnset":"The learnset of a pokemon. Checks if equated move is in the learnset (i.e learns=rock-throw)"
+}
+
+function evaluateArgument(/**@type String */arg, /**@type [PokeIcon] */dataList, formDict)
+{
+    arg = arg.toLowerCase();
+    console.log(dataList)
+    var keys = Object.keys(operators).sort(function(a, b){return b.length - a.length;});
+    var hasOperator = false;
+    var operator = "";
+    var comparator = ""
+    var value = "";
+    var possibleComps = ["=",">","<"]
+    console.log(keys);
+    for(var i = 0; i < keys.length; i++)
+    {
+        var k = keys[i];
+        if(arg.includes(k))
+        {
+            if(operator != "" && operators[k] == operators[operator]){continue;} //skip if it is an equivalent operator
+            console.log(k + " vs. " + operator);
+            if(hasOperator){return [];} //check if there are multiple operators
+            if(!hasOperator){hasOperator = true};  
+            if(arg[arg.indexOf(k)] == arg.length - 1){return [];} //check if operator is at end
+            if(arg[arg.indexOf(k)] == arg.length - 2){return [];} //check if comparator is at end
+            if(!possibleComps.includes(arg.charAt(arg.indexOf(k) + k.length))){return [];} //check if there is valid comparator
+            operator = k;
+            comparator = arg.charAt((arg.indexOf(k) + k.length));
+            var value = arg.substring((arg.indexOf(k) + k.length) + 1);
+        }
+    }
+    
+    if(!hasOperator){operator = "name"; comparator = ""; value = arg;}
+    var finalList = []
+    for(var i = 0; i < dataList.length; i++)
+    {
+        
+        if(operators[operator]({data: dataList[i][0], spec_data: dataList[i][1], forms: formDict[toString(dataList[i][3])]}, comparator, value))
+        {
+            finalList.push(dataList[i]);
+        }
+    }
+    return finalList;
+}
+
+module.exports = {evaluateArgument};
